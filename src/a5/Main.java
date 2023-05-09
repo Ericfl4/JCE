713a44b0ea5619732e3f7463a89d3f59c00f344b
Ericfl4 <fernandezlunaeric@elpuig.xeill.net>
2023-05-09 16:48:50 +0200

    hecho

--- src/a5/Main.java
+++ src/a5/Main.java
@@ -1,22 +1,26 @@
 package a5;
 
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.KeyPair;
+import java.security.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
         UtilitatsXifrar utilitatsXifrar = new UtilitatsXifrar();
+
+
+        //Ex 1:
+        /*
 
         KeyPair keyPair = utilitatsXifrar.randomGenerate(1024);
         System.out.println("Dime el mensaje");
@@ -31,5 +35,74 @@ public class Main {
         System.out.println("El mensaje es: "+newMensaje);
         System.out.println("La clave publica es: "+keyPair.getPublic().toString());
         System.out.println("La clave privada es: "+keyPair.getPrivate().toString());
+        */
+
+        //Ex 2:
+        KeyStore keyStore = utilitatsXifrar.loadKeyStore("C:/Users/ericf/mykeystore.jks","1Manzanar");
+        System.out.println("Tipo de keystore: " + keyStore.getType());
+        System.out.println("Cantidad de keys en la keystore: " + keyStore.size());
+
+        System.out.println("Alias de todas las keys en la keystore:");
+        Enumeration<String> aliases = keyStore.aliases();
+        while (aliases.hasMoreElements()) {
+            String alias = aliases.nextElement();
+            System.out.println(alias);
+        }
+
+        Enumeration<String> aliases2 = keyStore.aliases();
+        String alias = aliases2.nextElement();
+        String pas = "1Manzanar";
+        char[] pasChar = pas.toCharArray();
+        Key algorithm = keyStore.getKey(alias, pasChar);
+        System.out.println("Llave: "+alias+
+                ", Certificado: "+keyStore.getCertificate(alias)+
+                ", Algoritomo de cifrado: "+algorithm);
+
+
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256);
+        SecretKey secretKey = keyGen.generateKey();
+        KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection("1Manzanar".toCharArray());
+        KeyStore.SecretKeyEntry secretKeyEntry = new KeyStore.SecretKeyEntry(secretKey);
+
+        keyStore.setEntry("mykey3", secretKeyEntry, protParam);
+
+
+        PublicKey pk = utilitatsXifrar.getPublicKey(keyStore, "mykey", "1Manzanar");
+        System.out.println("Public key:");
+        System.out.println(pk.toString());
+
+
+
+
+
+        KeyPairGenerator keyGen2 = KeyPairGenerator.getInstance("RSA");
+        keyGen2.initialize(2048);
+        KeyPair keyPair2 = keyGen2.generateKeyPair();
+
+        PrivateKey privateKey = keyPair2.getPrivate();
+        PublicKey publicKey = keyPair2.getPublic();
+
+        byte[] data = "Hello world".getBytes();
+
+        byte[] signature = utilitatsXifrar.signData(data, privateKey);
+
+        System.out.println(signature.toString());
+
+        boolean verified = utilitatsXifrar.validateSignature(data, signature, publicKey);
+
+        if (verified) {
+            System.out.println("La firma "+signature+" es válida");
+        } else {
+            System.out.println("La firma "+signature+" no es válida");
+        }
+
+
+        String saludo = "Hola mariposa";
+        byte[][] encryptWD = utilitatsXifrar.encryptWrappedData(saludo.getBytes(),publicKey);
+        byte[] decryptWD = utilitatsXifrar.decryptWrappedData(encryptWD, privateKey);
+        System.out.println(decryptWD.toString());
+
+
     }
 }
--- src/a5/UtilitatsXifrar.java
+++ src/a5/UtilitatsXifrar.java
@@ -4,7 +4,13 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.util.Arrays;
 
 public class UtilitatsXifrar {
@@ -75,4 +81,103 @@ public class UtilitatsXifrar {
         }
         return keys;
     }
+
+    public KeyStore loadKeyStore(String ksFile, String ksPwd) throws Exception {
+
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        File f = new File (ksFile);
+        if (f.isFile()) {
+            FileInputStream in = new FileInputStream (f);
+            ks.load(in, ksPwd.toCharArray());
+        }
+        return ks;
+    }
+
+    public static PublicKey getPublicKey(String fitxer) throws FileNotFoundException, CertificateException {
+
+        FileInputStream fis = new FileInputStream(fitxer);
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        java.security.cert.Certificate cert = cf.generateCertificate(fis);
+        return cert.getPublicKey();
+
+    }
+
+    public static PublicKey getPublicKey(KeyStore ks, String alias, String pwMyKey) throws UnrecoverableKeyException,
+            KeyStoreException, NoSuchAlgorithmException {
+        Key key = ks.getKey(alias, pwMyKey.toCharArray());
+        if (key instanceof PrivateKey) {
+            Certificate cert = ks.getCertificate(alias);
+            PublicKey publicKey = cert.getPublicKey();
+            return publicKey;
+        }
+        return null;
+    }
+
+
+
+    public byte[] signData(byte[] data, PrivateKey priv) {
+        byte[] signature = null;
+
+        try {
+            Signature signer = Signature.getInstance("SHA1withRSA");
+            signer.initSign(priv);
+            signer.update(data);
+            signature = signer.sign();
+        } catch (Exception ex) {
+            System.err.println("Error signant les dades: " + ex);
+        }
+        return signature;
+    }
+
+    public Boolean validateSignature(byte[] data, byte[] signature, PublicKey pub) {
+        boolean isValid = false;
+        try {
+            Signature signer = Signature.getInstance("SHA1withRSA");
+            signer.initVerify(pub);
+            signer.update(data);
+            isValid = signer.verify(signature);
+        } catch (Exception ex) {
+            System.err.println("Error validant les dades: " + ex);
+        }
+        return isValid;
+    }
+
+    public byte[][] encryptWrappedData(byte[] data, PublicKey pub) {
+        byte[][] encWrappedData = new byte[2][];
+        try {
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128);
+            SecretKey sKey = kgen.generateKey();
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, sKey);
+            byte[] encMsg = cipher.doFinal(data);
+            cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.WRAP_MODE, pub);
+            byte[] encKey = cipher.wrap(sKey);
+            encWrappedData[0] = encMsg;
+            encWrappedData[1] = encKey;
+        } catch (Exception  ex) {
+            System.err.println("Ha succeït un error xifrant: " + ex);
+        }
+        return encWrappedData;
+    }
+
+    public static byte[] decryptWrappedData(byte[][] encWrappedData, PrivateKey priv) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.UNWRAP_MODE, priv);
+            SecretKey sKey = (SecretKey) cipher.unwrap(encWrappedData[1], "AES", Cipher.SECRET_KEY);
+            cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, sKey);
+            byte[] decMsg = cipher.doFinal(encWrappedData[0]);
+            return decMsg;
+        } catch (Exception ex) {
+            System.err.println("Ha succeït un error desxifrant: " + ex);
+            return null;
+        }
+    }
+
 }
+
+
+
